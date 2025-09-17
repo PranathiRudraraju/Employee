1225ea6399f6eb57699b47d4b8f594871e699027
PranathiRudraraju <rudrarajupranathi25@gmail.com>
2025-09-17 17:22:24 +0530

    Add STATIC_ROOT for collectstatic

--- Employee/settings.py
+++ Employee/settings.py
@@ -124,9 +124,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL='/'
 LOGIN_URL='login'
-
 import os
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
